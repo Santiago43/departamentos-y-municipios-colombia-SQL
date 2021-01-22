0e1e60f4288ed628517c1626fdc6274d92c9a137
Santiago43 <santipego0001@gmail.com>
2021-01-22 12:49:41 -0500

    Se ajustan tablas

--- departamentos.sql
+++ departamentos.sql
@@ -1,40 +1,5 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.6.33)
-# Database: colombia
-# Generation Time: 2016-12-15 11:55:40 +0000
-# ************************************************************
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table departamentos
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `departamentos`;
-
-CREATE TABLE `departamentos` (
-  `id_departamento` int(2) unsigned NOT NULL AUTO_INCREMENT,
-  `departamento` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_departamento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `departamentos` WRITE;
-/*!40000 ALTER TABLE `departamentos` DISABLE KEYS */;
-
-INSERT INTO `departamentos` (`id_departamento`, `departamento`)
+INSERT INTO Departamento (Departamento_ID, Nombre)
 VALUES
 	(5,'ANTIOQUIA'),
 	(8,'ATLÁNTICO'),
@@ -70,14 +35,3 @@ VALUES
 	(97,'VAUPÉS'),
 	(99,'VICHADA');
 
-/*!40000 ALTER TABLE `departamentos` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
